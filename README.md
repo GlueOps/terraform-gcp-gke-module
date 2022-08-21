4f8bf9b98a72dcecc2c91c21f83e94a7354da2c7
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-08-21 21:59:37 +0000

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # terraform-gcp-gke-module
+
+Terraform Module to deploy a Kubernetes/GKE Cluster into GCP (Google Cloud Platform)
+
+Usage
+```
+
+locals {
+  gcp_folder_id = "XXXXXXXXXXXX"
+}
+
+module "vpc" {
+  source = "git::https://github.com/GlueOps/terraform-gcp-gke-module.git"
+
+  workspace     = var.workspace
+  region        = "us-central1"
+  gcp_folder_id = local.gcp_folder_id
+}
+```
---
+++ variables.tf
@@ -0,0 +1,39 @@
+
+
+variable "workspace" {}
+
+variable "region" {
+  type        = string
+  default     = "us-central1"
+  description = "The GCP region to deploy these networks into"
+}
+
+
+variable "gcp_folder_id" {
+  type        = string
+  default     = ""
+  description = "The GCP Project Folder this project is within"
+}
+
+variable "control_panel_network" {
+  type        = string
+  default     = "10.64.96.16/28"
+  description = "Master Control Panel Network"
+}
+
+
+variable "kubernetes_version_prefix" {
+  type        = string
+  default     = "1.22.8-gke.200"
+  description = "Number of IPs we should manually allocate for Cloud NAT"
+}
+
+
+
+# data "google_container_engine_versions" "central1b" {
+#   provider       = google-beta
+#   location       = var.region
+#   version_prefix = var.kubernetes_version_prefix
+# }
+
+
