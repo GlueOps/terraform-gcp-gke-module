8c0f0e5489087c764580967d83a9832d6ade13a8
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-09-05 12:52:42 -0700

    Update nodepools.tf

--- nodepools.tf
+++ nodepools.tf
@@ -4,7 +4,7 @@ resource "google_container_node_pool" "name" {
 
   cluster = google_container_cluster.gke.id
 
-  initial_node_count = 2
+  initial_node_count = 3
 
   management {
     auto_upgrade = true
